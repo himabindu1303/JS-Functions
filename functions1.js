bad820899bf368412be5597b430be04de9f377c3
himabindu1303 <neellapalahimabindu@gmail.com>
2024-10-10 19:05:05 +0530

    functions in js

---
+++ functions1.js
@@ -0,0 +1,27 @@
+function addnum(a,b){                                                  //1st
+    console.log(a+b);      
+}
+addnum(50,100)
+
+
+function subtract(a,b) {                                               //2nd
+    c=b-a;
+    console.log(c);   
+}
+subtract(20,80)
+
+
+function multiply(a,b) {                                                //3rd
+    console.log(a*b);    
+}
+multiply(100,10)
+
+
+function divide(a,b) {                                                  //4th
+    if(b==0){
+        console.log("Cannot divide by zero.");    
+    }else{
+        console.log(a/b);
+    }    
+}
+divide(70,10)
